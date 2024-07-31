64cc77918baf6242c487b1fc3a0a11f36de34582
dhhan <dhhan@.covision.co.kr>
2024-07-31 18:15:44 +0900

    filedownload test

--- fileupload/src/main/java/com/example/fileupload/controller/TestController.java
+++ fileupload/src/main/java/com/example/fileupload/controller/TestController.java
@@ -3,7 +3,9 @@ package com.example.fileupload.controller;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -50,5 +52,36 @@ public class TestController {
             return "File Upload Failed";
         }
         
+    };
+
+    @RequestMapping(value = "/test/download", method = RequestMethod.POST)
+    public String testDownload(@RequestParam("filename") String filename, HttpServletResponse response){
+        String path = "D:/study_in_covision/vanilaHtml/file/";
+        File file = new File(path+filename);
+        if (!file.exists()) return "File Not Found";
+        
+        response.setContentType("application/octet-stream;charset=utf-8");
+        response.setContentLength(Integer.valueOf((int) file.length()));
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        //response.setHeader("Content-Disposition", FileHelper.getDisposition(fileDTO.getFileName(), FileHelper.getBrowser(request.getHeader("User-Agent"))));
+        response.setHeader("Content-Transfer-Encoding", "binary");
+
+        try (FileInputStream inputStream = new FileInputStream(file)) {
+            byte[] buffer = new byte[11410];
+            int read;
+            while ((read = inputStream.read(buffer))!= -1) {
+                response.getOutputStream().write(buffer,0,read);
+                response.getOutputStream().flush();
+            }
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+        return "File Download Success";
+
     }
+
+    // @RequestMapping(value = "/test/view", method = RequestMethod.GET, produces=MediaType.IMAGE_PNG_VALUE)
+    // public void testView(@RequestParam("filename") String filename, HttpServletResponse response){
+    //     response.setContentType("application/octet-stream");
+    //     response.setHeader("Content-Disposition", "attachment; filename="+filename);
 }
