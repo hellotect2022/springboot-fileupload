3dc91664eff2f31587921afd46d2ed45f8d8e76d
dhhan <dhhan@.covision.co.kr>
2024-08-01 13:26:22 +0900

    브라우저에서 파일다운로드 로직추가

--- fileupload/src/main/java/com/example/fileupload/controller/TestController.java
+++ fileupload/src/main/java/com/example/fileupload/controller/TestController.java
@@ -4,13 +4,18 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
@@ -54,34 +59,85 @@ public class TestController {
         
     };
 
-    @RequestMapping(value = "/test/download", method = RequestMethod.POST)
-    public String testDownload(@RequestParam("filename") String filename, HttpServletResponse response){
+    @RequestMapping(value = "/test/download", method = RequestMethod.GET)
+    public void testDownload(@RequestParam("filename") String filename, HttpServletResponse response, HttpServletRequest request) throws UnsupportedEncodingException{
         String path = "D:/study_in_covision/vanilaHtml/file/";
         File file = new File(path+filename);
-        if (!file.exists()) return "File Not Found";
-        
+        if (!file.exists()) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        System.out.println("file-lenght="+file.length());
         response.setContentType("application/octet-stream;charset=utf-8");
-        response.setContentLength(Integer.valueOf((int) file.length()));
+        //response.setContentType("text/plain;charset=utf-8");
+        response.setContentLength((int) file.length());
         response.setHeader("Access-Control-Allow-Origin", "*");
-        //response.setHeader("Content-Disposition", FileHelper.getDisposition(fileDTO.getFileName(), FileHelper.getBrowser(request.getHeader("User-Agent"))));
+        response.setHeader("Content-Disposition",getDisposition(filename, check_browser(request)));// 파일 강제 다운로드
         response.setHeader("Content-Transfer-Encoding", "binary");
 
         try (FileInputStream inputStream = new FileInputStream(file)) {
-            byte[] buffer = new byte[11410];
+            byte[] buffer = new byte[4096];
             int read;
             while ((read = inputStream.read(buffer))!= -1) {
                 response.getOutputStream().write(buffer,0,read);
-                response.getOutputStream().flush();
             }
+            response.getOutputStream().flush();
         }catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             System.out.println(e);
         }
-        return "File Download Success";
-
+        System.out.println("파일 다운로드 끝");
     }
 
     // @RequestMapping(value = "/test/view", method = RequestMethod.GET, produces=MediaType.IMAGE_PNG_VALUE)
     // public void testView(@RequestParam("filename") String filename, HttpServletResponse response){
     //     response.setContentType("application/octet-stream");
     //     response.setHeader("Content-Disposition", "attachment; filename="+filename);
+
+    private String check_browser(HttpServletRequest request) {
+        String browser = "";
+        String header = request.getHeader("User-Agent");
+        //신규추가된 indexof : Trident(IE11) 일반 MSIE로는 체크 안됨
+        if (header.indexOf("MSIE") > -1 || header.indexOf("Trident") > -1){
+            browser = "ie";
+        }
+        //크롬일 경우
+        else if (header.indexOf("Chrome") > -1){
+            browser = "chrome";
+        }
+        //오페라일경우
+        else if (header.indexOf("Opera") > -1){
+            browser = "opera";
+        }
+        //사파리일 경우
+        else if (header.indexOf("Apple") > -1){
+            browser = "sarari";
+        } else {
+            browser = "firfox";
+        }
+        return browser;
+    }
+ 
+    private String getDisposition(String down_filename, String browser_check) throws UnsupportedEncodingException {
+        String prefix = "attachment;filename=";
+        String encodedfilename = null;
+        System.out.println("browser_check:"+browser_check);
+        if (browser_check.equals("ie")) {
+            encodedfilename = URLEncoder.encode(down_filename, "UTF-8").replaceAll("\\+", "%20");
+        }else if (browser_check.equals("chrome")) {
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < down_filename.length(); i++){
+                char c = down_filename.charAt(i);
+                if (c > '~') {
+                    sb.append(URLEncoder.encode("" + c, "UTF-8"));
+                } else {
+                    sb.append(c);
+                }
+            }
+            encodedfilename = sb.toString();
+        }else {
+            encodedfilename = "\"" + new String(down_filename.getBytes("UTF-8"), "8859_1") + "\"";
+        }
+        return prefix + encodedfilename;
+    }
 }
